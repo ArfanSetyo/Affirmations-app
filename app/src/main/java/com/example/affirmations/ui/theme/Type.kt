48d70305fff94bd5faab08b44f102c0775000e90
ArfanSetyo <arfan.marsetyo@students.utdi.ac.id>
2022-12-12 11:48:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/ui/theme/Type.kt
@@ -0,0 +1,17 @@
+//menentukan jenis tipe//
+package com.example.affirmations.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Atur gaya tipografi Material untuk memulai
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+)
